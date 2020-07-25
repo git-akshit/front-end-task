8de58c84c9059ab618ce432cde39495d93ba8ff5
07aki <akshitverma@Akshits-MacBook-Pro.local>
2020-07-26 00:36:04 +0530

    Updates

---
+++ precache-manifest.d41eac04480e37a1800907faeaf0042f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d735185d3c0fbcaa70a30ca7537e4fd8",
+    "url": "/front-end-task/index.html"
+  },
+  {
+    "revision": "26d6a63cff2bdf006d66",
+    "url": "/front-end-task/static/css/main.074c155e.chunk.css"
+  },
+  {
+    "revision": "4dc6aed85b3b4019f003",
+    "url": "/front-end-task/static/js/2.dda66f4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/front-end-task/static/js/2.dda66f4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26d6a63cff2bdf006d66",
+    "url": "/front-end-task/static/js/main.4a9c5eaa.chunk.js"
+  },
+  {
+    "revision": "88a4331376cee0a58045",
+    "url": "/front-end-task/static/js/runtime-main.65e45c9e.js"
+  },
+  {
+    "revision": "61aec1412c3866de298757b470f9dad8",
+    "url": "/front-end-task/static/media/DINPro-Boldtr.61aec141.woff"
+  },
+  {
+    "revision": "bbed362ea4f479c32fe2044f100f6e53",
+    "url": "/front-end-task/static/media/DINPro-Mediumtr.bbed362e.woff"
+  },
+  {
+    "revision": "619ee97eae1c5e93f5d68a9157b2466c",
+    "url": "/front-end-task/static/media/college.619ee97e.png"
+  },
+  {
+    "revision": "8793ec0ee96ac6622dd5064036daac2a",
+    "url": "/front-end-task/static/media/college_01.8793ec0e.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/front-end-task/static/media/logo.5d5d9eef.svg"
+  }
+]);
